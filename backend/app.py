51af9f9c33f808bde89acc945e7dceb2df76ee07
Harit-21 <161210955+Harit-21@users.noreply.github.com>
2025-10-01 02:28:24 +0530

    devprod

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 nltk.download('punkt')
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://chaiaurchhat.vercel.app"]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": ["https://chaiaurchhat.vercel.app"]}}, supports_credentials=True)
 
 
 model_name = "sshleifer/distilbart-cnn-12-6"
